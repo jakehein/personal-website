9cccc8b843cca240a2777011a5e4b4485da17110
Jake Hein <jacobnolanhein@gmail.com>
2023-02-21 14:15:42 -0800

    improved navbar animations

--- src/components/navigation/NavElement.js
+++ src/components/navigation/NavElement.js
@@ -16,12 +16,37 @@ function NavElement(props) {
         as={NextLink}
         replace
         href={route}
-        display={"block"}
         width={"100%"}
         height={"100%"}
         textDecoration={"none"}
         padding={"0 .25em"}
         lineHeight={"2em"}
+        display="inline-block"
+        // animation specifics below
+        backgroundImage="linear-gradient(to right, #54b3d6, #54b3d6 50%, #FFF 50%)"
+        backgroundSize="200% 100%"
+        backgroundPosition="-100%"
+        position="relative"
+        backgroundClip={"text"}
+        textColor="transparent"
+        transition="all 0.5s ease-in-out"
+        _hover={{
+          backgroundPosition: "0",
+          "&::before": {
+            width: "100%",
+          },
+        }}
+        _before={{
+          content: "''",
+          background: "rgba(78,66,245,1)",
+          display: "block",
+          position: "absolute",
+          bottom: "3px",
+          left: "0",
+          width: "0",
+          height: "3px",
+          transition: "all 0.5s ease-in-out",
+        }}
       >
         {routeName}
       </Link>
